15f7b1e90c31258799c641917d6479772008bd78
Wishymashy <amoralesmm@yahoo.com>
2023-04-13 12:40:54 -0500

    Screen Windows setup

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import pygame
 pygame.init()
 
 #######Game Code########
-
+width = 800
+height = 899
+background_color = "black"
+game_window = pygame.display.set_mode((width, height))
+game_window.fill(background_color)  
+pygame.display.update()
 
 
 pygame.quit()
