6110da1b04701b235c4ba4cdd5adabc94fb66fe7
Wishymashy <amoralesmm@yahoo.com>
2023-04-13 19:44:22 -0500

    Added While loop and clock

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ pygame.init()
 
 #######Game Code########
 width = 800
-height = 899
+height = 800
 background_color = "black"
 game_window = pygame.display.set_mode((width, height))
-game_window.fill(background_color)  
-pygame.display.update()
 
+clock = pygame.time.Clock()
+
+while True:
+    game_window.fill(background_color)  
+    pygame.display.update()
+    
+    clock.tick(60)
+    
 
 pygame.quit()
 quit()
