6271aa71b837e5cf6e930486bf87482dea2d2911
WishmasterAku <xandertoolz@gmail.com>
2023-04-14 01:09:50 -0500

    Added Player

--- game.py
+++ game.py
@@ -10,17 +10,22 @@ class Game:
         self.height = 800
         self.background_color = "black"
         self.game_window = pygame.display.set_mode((self.width, self.height))
-
+        self.player = Player(375, 700, 50, 50, 'assets/assets/player.png', 10)
         self.clock = pygame.time.Clock()#set clock
 
         self.background = GameObject(0, 0, self.width, self.height, 'assets/assets/background.png')
+        self.player = Player(375, 700, 50, 50, 'assets/assets/player.png', 10)
         self.treasure = GameObject(375, 50, 50, 50, 'assets/assets/treasure.png')
 
+        
 
     def draw_objects(self):
         self.game_window.fill(self.background_color)  
+
         self.game_window.blit(self.background.image, (self.background.x, self.background.y))#Top Left Corner | 800 x 800 = Bottom Right Corner
-        self.game_window.blit(self.treasure.image, (self.treasure.x, self.treasure.y))            
+        self.game_window.blit(self.player.image, (self.player.x, self.player.y))
+        self.game_window.blit(self.treasure.image, (self.treasure.x, self.treasure.y))       
+
         pygame.display.update()   
 
     def run_game(self):#Method that belongs to Game Class so needs Self init to access variables
--- player.py
+++ player.py
@@ -1,15 +1,13 @@
 import pygame
 from gameObjects import GameObject
 
-class Player:
+class Player(GameObject):
 
-    def __init__(self, x, y, width, height, image_path):
-        image = pygame.image.load(image_path)
-        self.image = pygame.transform.scale(image, (width, height))
+    def __init__(self, x, y, width, height, image_path, speed):#init is a method that is called when an object is created from a class
+        super().__init__(x, y, width, height, image_path)#super() is used to inherit from parent class
 
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
+        self.speed = speed
 
-        self.velocity = 5
+    def move(self, direction):
+        self.y += (self.speed * direction)
+        
