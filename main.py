ec36be9e7cc6c59cd52c1994081b1defd51abb72
WishmasterAku <xandertoolz@gmail.com>
2023-04-13 21:53:08 -0500

    Run Game with proper Quit

--- main.py
+++ main.py
@@ -8,14 +8,20 @@ height = 800
 background_color = "black"
 game_window = pygame.display.set_mode((width, height))
 
-clock = pygame.time.Clock()
+clock = pygame.time.Clock()#set clock
 
-while True:
-    game_window.fill(background_color)  
-    pygame.display.update()
-    
-    clock.tick(60)
-    
+def run_game():
+    while True:
+        events = pygame.event.get()#get all events
+        for event in events:
+            if event.type == pygame.QUIT:#if user clicks close
+                return
 
+        game_window.fill(background_color)  
+        pygame.display.update()
+        
+        clock.tick(60)#set fps
+
+run_game()
 pygame.quit()
 quit()
