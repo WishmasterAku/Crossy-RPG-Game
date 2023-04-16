82f17c5cfa5d2c5f7aa2fa25617b9dfbc08e8233
WishmasterAku <xandertoolz@gmail.com>
2023-04-15 21:58:34 -0500

    Started Enemy script

---
+++ enemy.py
@@ -0,0 +1,16 @@
+import pygame
+from gameObjects import GameObject
+
+class Enemy(GameObject):
+
+    def __init__(self, x, y, width, height, image_path, speed):#init is a method that is called when an object is created from a class
+        super().__init__(x, y, width, height, image_path)#super() is used to inherit from parent class
+
+        self.speed = speed
+
+       
+
+    def move(self):
+
+        
+        self.x += self.speed
